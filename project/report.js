3ad9dbb8c2d64352b5943af1a54b3cfd9e88f36a
Mohammed0812 <79253151+Mohammed0812@users.noreply.github.com>
2021-02-24 15:45:26 +0530

    update

---
+++ project/report.js
@@ -0,0 +1,63 @@
+function apple() {
+    var table = document.querySelector('#table');
+    var q = parseInt(document.querySelector('#received').value);
+    document.querySelector('#received1').innerHTML = q;
+    var h = parseInt(document.querySelector('#onHand').value);
+    document.querySelector('#onHand1').innerHTML = h; 
+    var total1 = (q + h);
+    document.querySelector('#total1').innerHTML = total1;
+}
+function banana() {
+    var table = document.querySelector('#table');
+    var q = parseInt(document.querySelector('#received').value);
+    document.querySelector('#received2').innerHTML = q;
+    var h = parseInt(document.querySelector('#onHand').value);
+    document.querySelector('#onHand2').innerHTML = h; 
+    var total2 = (q + h);
+    document.querySelector('#total2').innerHTML = total2;
+}
+function mango() {
+    var table = document.querySelector('#table');
+    var q = parseInt(document.querySelector('#received').value);
+    document.querySelector('#received3').innerHTML = q;
+    var h = parseInt(document.querySelector('#onHand').value);
+    document.querySelector('#onHand3').innerHTML = h; 
+    var total3 = (q + h);
+    document.querySelector('#total3').innerHTML = total3;
+}
+function orange() {
+    var table = document.querySelector('#table');
+    var q = parseInt(document.querySelector('#received').value);
+    document.querySelector('#received4').innerHTML = q;
+    var h = parseInt(document.querySelector('#onHand').value);
+    document.querySelector('#onHand4').innerHTML = h; 
+    var total4 = (q + h);
+    document.querySelector('#total4').innerHTML = total4;
+}
+function lemon() {
+    var table = document.querySelector('#table');
+    var q = parseInt(document.querySelector('#received').value);
+    document.querySelector('#received5').innerHTML = q;
+    var h = parseInt(document.querySelector('#onHand').value);
+    document.querySelector('#onHand5').innerHTML = h; 
+    var total5 = (q + h);
+    document.querySelector('#total5').innerHTML = total5;
+}
+function pineApple() {
+    var table = document.querySelector('#table');
+    var q = parseInt(document.querySelector('#received').value);
+    document.querySelector('#received6').innerHTML = q;
+    var h = parseInt(document.querySelector('#onHand').value);
+    document.querySelector('#onHand6').innerHTML = h; 
+    var total6 = (q + h);
+    document.querySelector('#total6').innerHTML = total6;
+}
+function waterMelon() {
+    var table = document.querySelector('#table');
+    var q = parseInt(document.querySelector('#received').value);
+    document.querySelector('#received7').innerHTML = q;
+    var h = parseInt(document.querySelector('#onHand').value);
+    document.querySelector('#onHand7').innerHTML = h; 
+    var total7 = (q + h);
+    document.querySelector('#total7').innerHTML = total7;
+}
