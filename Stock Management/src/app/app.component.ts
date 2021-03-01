6ad5ceb88f22303425f5667bd37a367c65929034
Mohammed0812 <79253151+Mohammed0812@users.noreply.github.com>
2021-03-01 11:27:36 +0530

    Update app.component.ts

--- Stock Management/src/app/app.component.ts
+++ Stock Management/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-item',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
  title = 'hi';
- name = 'Apple';
- price = 100;
- add(): any{
-   this.name = 'Apple';
-   this.price = 100;
- }
+ 
 }
+
