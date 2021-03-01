b02d5197def1b7c768a3d45d03cae8f05560f402
Mohammed0812 <79253151+Mohammed0812@users.noreply.github.com>
2021-03-01 14:06:31 +0530

    update

--- Stock Management/src/app/app.component.ts
+++ Stock Management/src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-item',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- title = 'hi';
+  fname = "";
+  myclass = "sample-class-name";
+  fprice = 0;
+  fquant = 0;
+
+  product: any = [
+  ];
+
+  constructor() {
+    let a = localStorage.getItem('product');
+    if (a) {
+      this.product = JSON.parse(a);
+    }
+  }
+
+  addProduct() {
+    let f = {
+      name: this.fname,
+      price: this.fprice,
+      quant: this.fquant
+    };
+    this.product.push(f);
+    localStorage.setItem('product', JSON.stringify(this.product));
+  }
  
 }
-
--- Stock Management/src/app/app.module.ts
+++ Stock Management/src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ItemComponent } from './item/item.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
